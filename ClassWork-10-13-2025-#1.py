3135a3a5dfc384c3a451c31e879dd09b658f76e2
BraggD <braggd@jbu.edu>
2025-10-13 13:57:07 -0500

    LABWORK-3 10/10/2025

---
+++ ClassWork-10-13-2025-#1.py
@@ -0,0 +1,17 @@
+import pickle
+
+
+with open("cred_info","rb") as f:
+    load_dict = pickle.load(f)
+
+username=input("Enter Username: ")
+password=input("Enter Password: ")
+
+for user in load_dict.keys():
+    cred_name=load_dict[user]["username"]
+    cred_password=load_dict[user]["password"]
+    if cred_name == username and cred_password == password:
+        print("Credential Verified")
+        break
+
+f.close()

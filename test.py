e462d797d3739cd841b49baddaaebb5dfa442a5b
BraggD <braggd@jbu.edu>
2025-11-10 13:49:08 -0600

    11/10/2025 Classwork

--- test.py
+++ test.py
@@ -4,12 +4,13 @@ root = tkinter.Tk()
 root.resizable(False, False)
 myCanvas = tkinter.Canvas(root, bg="white", height=600, width=800)
 shape1 = myCanvas.create_oval(20, 20, 100, 100, fill="blue")
+shape2 = myCanvas.create_oval(20, 20, 100, 100, fill="red")
 #initialize the constants xx, yy
-xx =yy = 3
-cxx, xyy = xx, yy
+xx =yy = 0
+cxx, cyy = -xx, -yy
 
 def move_shape():
-    global xx, yy
+    global xx, yy, cxx, cyy
     #move the shape with the constanct xx, yy
     myCanvas.move(shape1, xx, yy)
     #get the current coordinates of the shape
@@ -19,6 +20,14 @@ def move_shape():
         xx = -xx
     if y1 <= 0 or y2 >= 600:
         yy = -yy
+
+    (cx1, cy1, cx2, cy2) = myCanvas.coords(shape2)
+    if cx1 <= 0 or cx2 >= 800:
+        cxx = -cxx
+    if cy1 <= 0 or cy2 >= 600:
+        cyy = -cyy
+
+    myCanvas.move(shape2, cxx, cyy)
     #call the function recursively
     myCanvas.after(30, move_shape)
     #in the main program call the function move_shape()
